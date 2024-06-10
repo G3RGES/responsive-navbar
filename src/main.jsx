2f7aaf0e4faa68451168f2f8cadd742eddadc1f4
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-10 09:13:22 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,43 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Blog from "./pages/Blog.jsx";
+import Services from "./pages/Services.jsx";
+import SignUp from "./pages/SignUp.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "blog",
+        element: <Blog />,
+      },
+      {
+        path: "services",
+        element: <Services />,
+      },
+      {
+        path: "signup",
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
