6c33eddab3adae1a9ae3f731292920cbd4c2097e
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-10 10:25:15 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { Logo } from "./Logo";
+import {
+  NavLinkWrapper,
+  NavbarWrapper,
+  StyledNavLink,
+  StyledFontAwesomeIcon,
+} from "../styles/Navbar.styled";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const links = [
+    {
+      page: "Home",
+      href: "/",
+    },
+    {
+      page: "About",
+      href: "/about",
+    },
+    {
+      page: "Blog",
+      href: "/blog",
+    },
+    {
+      page: "Services",
+      href: "/services",
+    },
+    {
+      page: "SignUp",
+      href: "/signup",
+    },
+  ];
+
+  const [active, setActive] = useState(false);
+
+  return (
+    <NavbarWrapper>
+      <Logo />
+      <StyledFontAwesomeIcon icon={faBars} onClick={() => setActive(!active)} />
+      <NavLinkWrapper active={active}>
+        {links.map((link, idx) => (
+          <StyledNavLink activeclassname="active" key={idx} to={link.href}>
+            {link.page}
+          </StyledNavLink>
+        ))}
+      </NavLinkWrapper>
+    </NavbarWrapper>
+  );
 };
 
 export default Navbar;
