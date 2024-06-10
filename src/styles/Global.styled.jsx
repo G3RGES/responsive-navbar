d10b664cb377806cd27b11a58c2fe303b4e12190
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-10 09:28:24 +0300

    Update Global.styled.jsx

--- src/styles/Global.styled.jsx
+++ src/styles/Global.styled.jsx
@@ -1,11 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
+const GlobalStyles = createGlobalStyle`
 body{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     font-family: 'Poppins',sans-serif;
 }
-
 `;
+
+export default GlobalStyles;
